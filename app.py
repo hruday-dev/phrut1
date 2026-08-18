4c5a8586cd8bdbd316bf6bf763dc8a2b83d43dc9
Hruday <thirumala8374@gmail.com>
2026-08-19 00:27:46 +0530

    Use environment variable for server port
    Updated app.run to use environment variable for port.

--- app.py
+++ app.py
@@ -818,4 +818,7 @@ with app.app_context():
     init_orders_csv()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get('PORT', 5000))
+    )
